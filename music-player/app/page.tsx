cdca12aa9c04e3698b882c5eac81b1e067ce3d65
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2025-01-21 05:06:35 +0000

    test1

---
+++ music-player/app/page.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+
+
+
+const TestView:NextPage = () => {
+  	return (
+    		<div className="w-full relative rounded-corner-large bg-gray h-[53.25rem] overflow-hidden text-left text-[8.891rem] text-silver font-inter">
+      			<div className="absolute top-[20.5rem] left-[2.5rem] [backdrop-filter:blur(4px)] rounded-corner-large bg-dimgray border-darkgray border-[2px] border-solid flex flex-row items-center justify-center p-[0.625rem]">
+        				<div className="relative">Test</div>
+      			</div>
+    		</div>);
+};
+
+export default TestView;
--- music-player/tailwind.config.ts
+++ music-player/tailwind.config.ts
@@ -6,42 +6,27 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        "white": "#fff",
-        "darkslategray": {
-          "100": "#3d3d3d",
-          "200": "#323232",
-          "300": "#2e2d2d"
-        },
-        "darkgray": "#a4a4a4",
-        "gray": {
-          "100": "#fdfdfd",
-          "200": "rgba(255, 255, 255, 0.5)"
-        }
+  "theme": {
+    "extend": {
+      "colors": {
+        "gray": "#1d1c1c",
+        "dimgray": "rgba(96, 96, 96, 0.31)",
+        "darkgray": "rgba(162, 162, 162, 0.59)",
+        "silver": "#bababa"
       },
       "spacing": {},
       "fontFamily": {
         "inter": "Inter"
       },
       "borderRadius": {
-        "20xl-3": "39.3px",
-        "981xl": "1000px",
-        "corner-large": "16px",
-        "corner-full": "1000px"
+        "corner-large": "16px"
       }
     },
     "fontSize": {
-      "lg-3": "1.144rem",
-      "xl": "1.25rem",
       "inherit": "inherit"
     }
   },
   "corePlugins": {
     "preflight": false
-  },
-  plugins: [],
+  }
 } satisfies Config;
